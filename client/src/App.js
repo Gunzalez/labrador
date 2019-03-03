af9200034089597e62351c7c0109791b0386ca22
Segun Konibire <gunzalez@hotmail.com>
2019-03-03 21:34:08 +0000

    adds basic routing and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import Bill from './components/Statement/Bill/Bill';
+import Contact from './components/Statement/Contact/Contact';
+import StatementContainer, { NavigationBar, NavigationBarItem } from './components/Statement.styles';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <StatementContainer>
+                    <h2>Your Energy Statement</h2>
+                    <NavigationBar>
+                        <NavigationBarItem><Link to={'/'} className="nav-link"> Main Bill </Link></NavigationBarItem>
+                        <NavigationBarItem><Link to={'/contact'} className="nav-link">Contact</Link></NavigationBarItem>
+                    </NavigationBar>
+                    <hr />
+                    <Switch>
+                        <Route exact path='/' component={Bill} />
+                        <Route path='/contact' component={Contact} />
+                    </Switch>
+                </StatementContainer>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/Statement/Bill/Bill.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Bill extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Bill</h2>
+            </div>
+        );
+    }
+}
+
+export default Bill;
